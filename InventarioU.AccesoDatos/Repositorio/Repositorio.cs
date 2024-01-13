20728824b89f8a741b67c513b11f29ef18ae549b
gian90gf <gian90gf@gmail.com>
2024-01-13 01:06:31 -0500

    Repositorio Generico

---
+++ InventarioU.AccesoDatos/Repositorio/Repositorio.cs
@@ -0,0 +1,96 @@
+﻿using InventarioU.AccesoDatos.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioU.AccesoDatos.Repositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+
+        }
+        public async Task Agregar(T entidad)
+        {
+            await dbSet.AddAsync(entidad);    
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTrancking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query.Where(filtro);
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query.Include(incluirProp);
+
+
+                }
+            }
+           
+            if (!isTrancking)
+            {
+                query = query.AsNoTracking();
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTrancking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query.Where(filtro);
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query.Include(incluirProp);
+
+
+                }
+            }
+            if(orderBy != null)
+            {
+                query=orderBy(query);
+            }
+            if (!isTrancking)
+            {
+                query = query.AsNoTracking();
+            }
+            return  await query.ToListAsync();
+        }
+
+        public void Remover(T entidad)
+        {
+            dbSet.Remove(entidad);
+        }
+
+        public void RemoverRango(IEnumerable<T> entidad)
+        {
+            dbSet.RemoveRange(entidad);
+        }
+    }
+}
